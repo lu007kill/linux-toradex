foundriesio/linux

pcie-imx6: add reset function for reseting downstream EP
With the following dtb node one can define a gpio to reset
downstream endpoints.
reset-ep-gpio = <...>;
Currently the logic is 1 for reset asserted
and 0 for reset deasserted.

Some pcie switches require their downstream endpoints to be kept in reset
for an additonal millisecond after their reset has been deasserted.

drivers/pci/host/pci-imx6.c
@@ -44,6 +44,7 @@ static u32 test_region_size = SZ_2M;
 
 struct imx6_pcie {
 	int			reset_gpio;
+	int			reset_ep_gpio;
 	int			power_on_gpio;
 	int			wake_up_gpio;
 	int			disable_gpio;
@@ -281,9 +282,14 @@ static int imx6_pcie_deassert_core_reset(struct pcie_port *pp)
 
 	/* Some boards don't have PCIe reset GPIO. */
 	if (gpio_is_valid(imx6_pcie->reset_gpio)) {
+		if (gpio_is_valid(imx6_pcie->reset_ep_gpio))
+			gpio_set_value(imx6_pcie->reset_ep_gpio, 1);
 		gpio_set_value(imx6_pcie->reset_gpio, 0);
 		msleep(100);
 		gpio_set_value(imx6_pcie->reset_gpio, 1);
+		msleep(1);
+		if (gpio_is_valid(imx6_pcie->reset_ep_gpio))
+			gpio_set_value(imx6_pcie->reset_ep_gpio, 0);
 	}
 	return 0;
 
@@ -752,6 +758,17 @@ static int __init imx6_pcie_probe(struct platform_device *pdev)
 					"PCIe reset");
 		if (ret) {
 			dev_err(&pdev->dev, "unable to get reset gpio\n");
+			return ret;
+		}
+	}
+	imx6_pcie->reset_ep_gpio = of_get_named_gpio(np, "reset-ep-gpio", 0);
+	if (gpio_is_valid(imx6_pcie->reset_ep_gpio)) {
+		ret = devm_gpio_request_one(&pdev->dev,
+					imx6_pcie->reset_ep_gpio,
+					GPIOF_OUT_INIT_HIGH,
+					"PCIe EP reset");
+		if (ret) {
+			dev_err(&pdev->dev, "unable to get reset end point gpio\n");
 			return ret;
 		}
 	}

lu007kill/linux-toradex

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
Acked-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
(cherry picked from commit 94a60e7f645965b1e422e4e80aa8ccb9e0ec845c)
(cherry picked from commit f2bb337d37acd680404371d2530a486bfe821ce2)
Conflicts:
	drivers/pci/host/pci-imx6.c
(cherry picked from commit 557f0276856f1731264492ad82fe2f13e2d64831)

drivers/pci/host/pci-imx6.c
@@ -62,6 +62,7 @@ struct imx6_pcie {
 	int			power_on_gpio;
 	int			reset_gpio;
 	bool			gpio_active_high;
+	int			reset_ep_gpio;
 	struct clk		*pcie_bus;
 	struct clk		*pcie_inbound_axi;
 	struct clk		*pcie_phy;
@@ -532,11 +533,16 @@ static void imx6_pcie_deassert_core_reset(struct imx6_pcie *imx6_pcie)
 
 	/* Some boards don't have PCIe reset GPIO. */
 	if (gpio_is_valid(imx6_pcie->reset_gpio)) {
+		if (gpio_is_valid(imx6_pcie->reset_ep_gpio))
+			gpio_set_value_cansleep(imx6_pcie->reset_ep_gpio, 1);
 		gpio_set_value_cansleep(imx6_pcie->reset_gpio,
 					imx6_pcie->gpio_active_high);
 		mdelay(20);
 		gpio_set_value_cansleep(imx6_pcie->reset_gpio,
 					!imx6_pcie->gpio_active_high);
+		mdelay(1);
+		if (gpio_is_valid(imx6_pcie->reset_ep_gpio))
+			gpio_set_value_cansleep(imx6_pcie->reset_ep_gpio, 0);
 		mdelay(20);
 	}
 
@@ -1221,6 +1227,17 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 					    GPIOF_OUT_INIT_HIGH, "PCIe DIS");
 		if (ret) {
 			dev_err(&pdev->dev, "unable to get disable gpio\n");
+			return ret;
+		}
+	}
+	imx6_pcie->reset_ep_gpio = of_get_named_gpio(node, "reset-ep-gpio", 0);
+	if (gpio_is_valid(imx6_pcie->reset_ep_gpio)) {
+		ret = devm_gpio_request_one(&pdev->dev,
+					imx6_pcie->reset_ep_gpio,
+					GPIOF_OUT_INIT_HIGH,
+					"PCIe EP reset");
+		if (ret) {
+			dev_err(&pdev->dev, "unable to get reset end point gpio\n");
 			return ret;
 		}
 	}